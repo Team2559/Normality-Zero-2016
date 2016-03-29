66513be605d0a4ce6ae46a0643308f5ebbaa4bbd
Andy Turner <turner.andy@gmail.com>
2016-03-29 15:12:46 -0400

    Added movement controller that will slow down as it nears the target. This will help reduce the overshoot due to robot momentum. The parameters of when and how much to slow down might need altering somewhat -  i.e. maybe start slowing down earlier, or slowing down even more than 0.5x speed.
    Also, when it reaches the target it will wait for some time (100ms
    currently) for the robot to settle, and then re-enable the controller
    for a short time (150ms currently). This will wait for any overshoot to
    happen (which should be less now anyway) and then start moving again at
    the slow speed to better align. The time to settle may need altering. If
    the slow down code works then the additional re-enable of the controller
    might not do much, but it's only 0.25 of a second. Depending on
    performance the re-enable may need even more slow down, like 0.1 during
    re-enable period.

---
+++ Stronghold2559_Main/src/org/usfirst/frc/team2559/lib/PIDTurnController.java
@@ -0,0 +1,181 @@
+package org.usfirst.frc.team2559.lib;
+
+/**
+ * A PID controller for turning the robot. It will slow down as it approaches
+ * the correct angle to account for the momentum of the robot
+ * 
+ * @author Andy
+ *
+ */
+public class PIDTurnController extends PIDControllerRT {
+	// How close we need to get to start slowing down;
+	private double outerThreshold = 8.0;
+	private double innerThreshold = 4.0;
+
+	// How much to slow down. 0.5=50% slow down, 0.2=20% slow down etc.
+	private double outerSlowdown = 0.5;
+	private double innerSlowdown = 0.2;
+
+	// If we have factored in the slowdown yet
+	private boolean startedInnerSlowdown = false;
+	private boolean startedOuterSlowdown = false;
+
+	// Orig values before we make them smaller
+	private double origKp = 0;
+	private double origKi = 0;
+	private double origKd = 0;
+	private double origMaxoutput_low = 0;
+	private double origMaxoutput_high = 0;
+
+	// After we have finished, allow the bot to settle and then enable control
+	// again for a short period
+	private long settleTimeMs = 100;
+	private long currentSettleTimeMs = 0;
+	private long maxRetryTimeMs = 150;
+	private long currentRetryTimeMs = 0;
+	private boolean isNormallyDone = false;
+
+	/**
+	 * 
+	 * @param P
+	 * @param I
+	 * @param D
+	 * @param outputmax_low
+	 * @param outputmax_high
+	 * @param threshold
+	 * @param useSmartTime
+	 * @param outerThreshold The value at which to start slowing down the controller
+	 * @param innerThreshold The value at which to start slowing down the controller even more
+	 * @param outerSlowdown The factor to slow down the controller
+	 * @param innerSlowdown The factor to slow down the controller even more
+	 */
+	public PIDTurnController(double P, double I, double D, double outputmax_low, double outputmax_high,
+			double threshold, boolean useSmartTime, double outerThreshold, double innerThreshold, double outerSlowdown,
+			double innerSlowdown) {
+		this(P, I, D, outputmax_low, outputmax_high, threshold, useSmartTime);
+
+		this.outerThreshold = outerThreshold;
+		this.innerThreshold = innerThreshold;
+		this.outerSlowdown = outerSlowdown;
+		this.innerSlowdown = innerSlowdown;
+	}
+
+	public PIDTurnController(double P, double I, double D, double outputmax_low, double outputmax_high,
+			double threshold, boolean useSmartTime) {
+		super(P, I, D, outputmax_low, outputmax_high, threshold, useSmartTime);
+
+		this.origKp = Kp;
+		this.origKi = Ki;
+		this.origKd = Kd;
+		this.origMaxoutput_low = maxoutput_low;
+		this.origMaxoutput_high = maxoutput_high;
+	}
+
+	public PIDTurnController(double P, double I, double D, double threshold, boolean useSmartTime) {
+		this(P, I, D, -1.0, 1.0, threshold, useSmartTime);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.usfirst.frc.team2559.lib.PIDControllerRT#calculate(double,
+	 * boolean)
+	 */
+	public void calculate(double cur_input, boolean clamp) {
+		long currentLastCalledTime = lastCalledTime;
+		
+		doSlowdown(cur_input);
+
+		// Now do the calculation as normal (maybe with slowed down values)
+		super.calculate(cur_input, clamp);
+
+		// If the controller is normally done we might want to wait for the
+		// robot to settle. We will set the output to 0.0 to settle
+		if (this.isNormallyDone && currentSettleTimeMs < settleTimeMs) {
+			output_value = 0.0;
+
+			// reverse any integral windup for this period
+			if (!preventWindUp) {
+				double cur_error = (setpoint - cur_input);
+				integral -= cur_error * dt;
+			}
+
+			// Add some time to the settle time (at least 1ms)
+			currentSettleTimeMs += Math.max(1l, System.currentTimeMillis() - currentLastCalledTime);
+		}
+		// Otherwise, if we would normally be finished by we are doing the after
+		// settle control increment that time
+		else if (this.isNormallyDone) {
+			currentRetryTimeMs += Math.max(1l, System.currentTimeMillis() - currentLastCalledTime);;
+		}
+	}
+
+	/**
+	 * If the current error is within one of the thresholds then slow down
+	 * @param cur_input
+	 */
+	private void doSlowdown(double cur_input) {
+		double cur_error = (setpoint - cur_input);
+
+		// Do the outer slowdown
+		if (!startedOuterSlowdown && Math.abs(cur_error) < outerThreshold) {
+			Kp = this.origKp * outerSlowdown;
+			Ki = this.origKi * outerSlowdown;
+			Kd = this.origKd * outerSlowdown;
+
+			maxoutput_low = this.origMaxoutput_low * outerSlowdown;
+			maxoutput_high = this.origMaxoutput_high * outerSlowdown;
+
+			this.startedOuterSlowdown = true;
+		}
+
+		// Do the inner slowdown
+		if (!startedInnerSlowdown && Math.abs(cur_error) < innerThreshold) {
+			Kp = this.origKp * innerSlowdown;
+			Ki = this.origKi * innerSlowdown;
+			Kd = this.origKd * innerSlowdown;
+
+			maxoutput_low = this.origMaxoutput_low * innerSlowdown;
+			maxoutput_high = this.origMaxoutput_high * innerSlowdown;
+
+			this.startedInnerSlowdown = true;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.usfirst.frc.team2559.lib.PIDControllerRT#isDone()
+	 */
+	public boolean isDone() {
+		boolean superDone = super.isDone();
+
+		if (superDone)
+			isNormallyDone = true;
+
+		return isNormallyDone && (currentRetryTimeMs >= maxRetryTimeMs);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.usfirst.frc.team2559.lib.PIDControllerRT#reset()
+	 */
+	public void reset() {
+		super.reset();
+
+		// Reset the slowdown
+		this.startedInnerSlowdown = false;
+		this.startedOuterSlowdown = false;
+
+		Kp = this.origKp;
+		Ki = this.origKi;
+		Kd = this.origKd;
+		maxoutput_low = this.origMaxoutput_low;
+		maxoutput_high = this.origMaxoutput_high;
+
+		// Reset the settle time
+		currentSettleTimeMs = 0;
+		currentRetryTimeMs = 0;
+		isNormallyDone = false;
+	}
+}
