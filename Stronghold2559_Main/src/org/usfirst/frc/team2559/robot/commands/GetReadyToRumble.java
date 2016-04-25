10769ad56fe619075a4d253d98d2dc192c372176
Andy Turner <turner.andy@gmail.com>
2016-04-25 17:43:39 -0400

    does this work for creating new commands?

--- Stronghold2559_Main/src/org/usfirst/frc/team2559/robot/commands/GetReadyToRumble.java
+++ Stronghold2559_Main/src/org/usfirst/frc/team2559/robot/commands/GetReadyToRumble.java
@@ -11,60 +11,81 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class GetReadyToRumble extends CommandGroup {
-    
+	CommandGroup cg = null;
     int val;
 
     public GetReadyToRumble(int val) {
-	this.val = val;
+    	this.val = val;
+    }
+    
+    @Override
+	public void execute(){
+    	cg = new CommandGroup();
 	
 	 System.out.println(Robot._arm.getArmAngle());
 	 if (Robot._arm.getArmAngle() < 35.0) {
-	 addSequential(new PIDSetArm(RobotMap.ARM_INITIAL_POS_ANGLE));
+	 cg.addSequential(new PIDSetArm(RobotMap.ARM_INITIAL_POS_ANGLE));
 	 }
 	
 	 // Do we need a delay?
 	
 	 if (val == RobotMap.PORTCULLIS_ID || val == RobotMap.DRAWBRIDGE_ID || val == RobotMap.SALLYPORT_ID) {
-	 addSequential(new PIDSetShooter(10));
+	 cg.addSequential(new PIDSetShooter(10));
 	 // shooter_setting = 10;
 	 } else if (val == RobotMap.CDF_ID || val == RobotMap.MOAT_ID || val == RobotMap.RAMPARTS_ID || val == RobotMap.ROCKWALL_ID || val
 	 == RobotMap.ROUGHTERRAIN_ID) {
-	 addSequential(new PIDSetShooter(40.5));
+	 cg.addSequential(new PIDSetShooter(40.5));
 	 // shooter_setting = 60;
 	 } else if (val == RobotMap.LOWBAR_ID) {
-	 addSequential(new PIDSetShooter(1));
+	 cg.addSequential(new PIDSetShooter(1));
 	 } else if (val == RobotMap.PUSHUP_ENDGAME_ID) {
-	 addSequential(new PIDSetShooter(30));
+	 cg.addSequential(new PIDSetShooter(30));
 	 } else if (val == RobotMap.HOME_ID) {
-	 addSequential(new PIDSetShooter(35));
+	 cg.addSequential(new PIDSetShooter(35));
 	 } else if (val == RobotMap.SHOOTING_ID) {
-	 addSequential(new PIDSetShooter(30));
+	 cg.addSequential(new PIDSetShooter(30));
 	 } else if (val == RobotMap.LOWGOAL_SHOOTING_ID) {
-	 addSequential(new PIDSetShooter(0));
+	 cg.addSequential(new PIDSetShooter(0));
 	 }
 	
 	 if (val == RobotMap.PORTCULLIS_ID || val == RobotMap.DRAWBRIDGE_ID || val == RobotMap.SALLYPORT_ID) {
-	 addSequential(new PIDSetArm(200));
+	 cg.addSequential(new PIDSetArm(200));
 	 // arm_setting= 0;
 	 } else if (val == RobotMap.LOWBAR_ID) {
 	 // set arms as far back as possible
-	 addSequential(new PIDSetArm(0));
+	 cg.addSequential(new PIDSetArm(0));
 	 // arm_setting=51;
 	 } else if (val == RobotMap.MOAT_ID || val == RobotMap.RAMPARTS_ID || val == RobotMap.ROCKWALL_ID || val ==
 	 RobotMap.ROUGHTERRAIN_ID) {
-	 addSequential(new PIDSetArm(145));
+	 cg.addSequential(new PIDSetArm(145));
 	 // arm_setting=41;
 	 } else if (val == RobotMap.CDF_ID) {
 	 // set arms in the middle
-	 addSequential(new PIDSetArm(145));
+	 cg.addSequential(new PIDSetArm(145));
 	 // arm_setting=8;
 	 } else if (val == RobotMap.PUSHUP_ENDGAME_ID) {
-	 addSequential(new PIDSetArm(210, -0.8, 0.8));
+	 cg.addSequential(new PIDSetArm(210, -0.8, 0.8));
 	 } else if (val == RobotMap.HOME_ID || val == RobotMap.LOWGOAL_SHOOTING_ID) {
-	 addSequential(new PIDSetArm(RobotMap.ARM_INITIAL_POS_ANGLE));
+	 cg.addSequential(new PIDSetArm(RobotMap.ARM_INITIAL_POS_ANGLE));
 	 } else if (val == RobotMap.SHOOTING_ID) {
-	 addSequential(new PIDSetArm(RobotMap.ARM_INTAKE_ANGLE));
+	 cg.addSequential(new PIDSetArm(RobotMap.ARM_INTAKE_ANGLE));
 	 }
+	 
+	 // Now run the command group we created
+	 cg.start();
+    }
+    
+    protected void end() {
+    	// cannot quite handle correctly
+    	if(cg!=null)
+    		cg.cancel();
+    }
+
+    // Can be overwritten by teams
+    protected void interrupted() {
+    	// cannot quite handle correctly
+    	if(cg!=null)
+    		cg.cancel(); 	
     }
     
     protected void initialize() {
